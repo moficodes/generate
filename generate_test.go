1400fc1117911fc1d2d912909136230dc3c1e194
Valentin Deleplace <deleplace@google.com>
2023-02-09 16:26:09 +0100

    Benchmark: write to memory, write to file.

---
+++ generate_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"testing"
+)
+
+func BenchmarkWrite10_000_seq(b *testing.B) {
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		var buffer bytes.Buffer
+		write(ctx, &buffer, 1, 10_000)
+	}
+}
+
+func BenchmarkWriteFile10_000_seq(b *testing.B) {
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		tmpfile, err := os.CreateTemp("", "")
+		if err != nil {
+			b.Fatal(err)
+		}
+		filename := tmpfile.Name()
+
+		writeToFile(ctx, filename, 1, 10_000)
+	}
+}
